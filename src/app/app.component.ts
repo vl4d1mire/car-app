7b6e6608b41722d8a8275c9b8cb2ad8932158051
vl4d1mire <67436834+vl4d1mire@users.noreply.github.com>
2023-01-15 20:02:03 +0300

    Functionality of receiving machine data through the server and validation of the form on the server was implementedÐ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -10,59 +11,22 @@ export class AppComponent {
   orderForm = this.fb.group({
     name: ['', Validators.required],
     phone: ['', Validators.required],
-    text: ['', Validators.required],
+    car: ['', Validators.required],
   })
 
-  carsData = [
-    {
-      image: '1.png',
-      name: 'Lamborghini Huracan Spyder',
-      transmission: 'Автомат',
-      engine: '5.2 л.с.',
-      year: '2019'
-    },
-    {
-      image: '2.png',
-      name: 'Chevrolet Corvette',
-      transmission: 'Автомат',
-      engine: '.2 л.с.',
-      year: '2017'
-    },
-    {
-      image: '3.png',
-      name: 'Ferrari California',
-      transmission: 'Автомат',
-      engine: '3.9 л.с.',
-      year: '2010'
-    },
-    {
-      image: '4.png',
-      name: 'Lamborghini Urus',
-      transmission: 'Автомат',
-      engine: '4 л.с.',
-      year: '2019'
-    },
-    {
-      image: '5.png',
-      name: 'Audi R8',
-      transmission: 'Автомат',
-      engine: '5.2 л.с.',
-      year: '2019'
-    },
-    {
-      image: '6.png',
-      name: 'Maserati Quattroporte',
-      transmission: 'Автомат',
-      engine: '3.0 л.с.',
-      year: '2018'
-    },
-  ]
-  constructor(private fb: FormBuilder) {
+  carsData: any
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carData => {
+      this.carsData = carData
+    })
   }
   scrollPage(target: HTMLElement, car?: any) {
     target.scrollIntoView({behavior: 'smooth'})
     if (car) {
-      this.orderForm.patchValue({text: car.name})
+      this.orderForm.patchValue({car: car.name})
     }
   }
 
@@ -79,8 +43,18 @@ export class AppComponent {
   }
   onSubmit() {
     if (this.orderForm.valid) {
-      alert('Спасибо за заявку, мы свяжемся с вами в ближайшее время')
+      this.appService.sendQuery(this.orderForm.value)
+        .subscribe(
+          {
+            next: (resp: any) => {
+              alert(resp.message);
+              this.orderForm.reset()
+            },
+            error: (resp: any) => {
+              alert(resp.error.message)
+            }
+          }
+        )
     }
-    this.orderForm.reset()
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -11,8 +10,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
